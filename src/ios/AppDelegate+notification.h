8c3713944dc2da6fd6a8d81f2cb78a3533906221
Carlos Mac <carlos.costa.cruz@gmail.com>
2019-03-13 01:09:56 +0000

    sev

---
+++ src/ios/AppDelegate+notification.h
@@ -0,0 +1,5 @@
+#import "AppDelegate.h"
+
+@interface AppDelegate (notification)
+- (void) registerPushNotifications;
+@end
---
+++ src/ios/AppDelegate+notification.m
@@ -0,0 +1,11 @@
+#import "AppDelegate+notification.h"
+#import <objc/runtime.h>
+
+@implementation AppDelegate (notification)
+
+- (void) registerPushNotifications
+{
+  // test
+}
+
+@end
