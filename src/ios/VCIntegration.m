9d853072af1e29cc22fe0edf612ba49ced86e6b5
Askblue <carlos.costa.cruz@gmail.com>
2019-03-15 01:26:02 +0000

    Update VCIntegration.m

--- src/ios/VCIntegration.m
+++ src/ios/VCIntegration.m
@@ -37,25 +37,25 @@
       NSString* serverName = [[command arguments] objectAtIndex:0];
     //  NSString* userMail = [[command arguments] objectAtIndex:1];
     //  NSString* passWord = [[command arguments] objectAtIndex:2];
-    //  NSString* personalID = [[command arguments] objectAtIndex:1];
+      NSString* personalID = [[command arguments] objectAtIndex:1];
       //NSString* personalID = [options objectAtIndex:1];
     //  NSString* personalID = [arguments objectAtIndex:1];
 
       NSLog(@"VC - VCIntegration1 %@", serverName);
     //  NSLog(@"VC - VCIntegration2 %@", userMail);
       //NSLog(@"VC - VCIntegration3 %@", passWord);
-      //NSLog(@"VC - VCIntegration4 %@", personalID);
+      NSLog(@"VC - VCIntegration4 %@", personalID);
 
 
       CDVPluginResult* result = [CDVPluginResult
                                  resultWithStatus:CDVCommandStatus_OK
-                                 messageAsString:msg];
+                                 messageAsString:@"ok"];
 
      UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Testing"
                                        message:@"Im in the Cordova plugin"
                                       delegate:nil cancelButtonTitle:@"ok" otherButtonTitles: nil];
 
-     [alert show];
+     //[alert show];
 
      _storyboardName = @"Main1";
 
