19a57dc39e23c936d88a0e653acae5526d5859e8
Askblue <carlos.costa.cruz@gmail.com>
2019-03-30 17:17:32 +0000

    Update CallActivity.java

--- src/android/CallActivity.java
+++ src/android/CallActivity.java
@@ -466,11 +466,11 @@ public class CallActivity extends AppCompatActivity implements SptCallFragment.O
               });
           }
 
-      //    @Override
-      //    public void requestUserAutorization(Intent intent, int i) {
-      //        _screenSharingRequestCode = i;
-      //        startActivityForResult(intent, i);
-      //    }
+          @Override
+          public void requestUserAutorization(Intent intent, int i) {
+              _screenSharingRequestCode = i;
+              startActivityForResult(intent, i);
+          }
       }
 
 }
