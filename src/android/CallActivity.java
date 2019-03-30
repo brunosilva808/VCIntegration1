2351b1999e3a3f70607ee6d81557c0b37a0edcf2
Askblue <carlos.costa.cruz@gmail.com>
2019-03-30 16:00:51 +0000

    Update CallActivity.java

--- src/android/CallActivity.java
+++ src/android/CallActivity.java
@@ -8,6 +8,7 @@ import android.media.AudioManager;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
